achur/django-icanhaz

Allow the use of regular expressions in icanhaz template tag
Allow users to put template tags like

  {% icanhaz './comments/' '.*' %}

which match all comments/[regex match].html where [regex match] is
then used as the template name for each and all are added to the
templatized page.

Also include some convenience finders to automatically scope regex-
matched templates.  In the above example, the names would then
be comments_[regex match].

icanhaz/conf.py
@@ -22,6 +22,10 @@ conf = Configuration(
         "icanhaz.finders.FilesystemFinder",
         "icanhaz.finders.AppFinder",
         ],
+    ICANHAZ_REGEX_FINDERS=[
+        "icanhaz.finders.FilesystemRegexFinder",
+        "icanhaz.finders.AppRegexFinder",
+        ],
     ICANHAZ_DIRS=[],
     ICANHAZ_APP_DIRNAMES=["jstemplates"],
     )

mjumbewu/django-jstemplate

Conflicts:
	README.rst
	icanhaz/templatetags/icanhaz.py
	mustachejs/conf.py
	mustachejs/loading.py

mustachejs/conf.py
@@ -27,4 +27,8 @@ conf = Configuration(
     MUSTACHEJS_APP_DIRNAMES=["jstemplates"],
     MUSTACHEJS_I18N_TAGS=["_", "i18n"],
     MUSTACHEJS_PREPROCESSORS=['mustachejs.preprocessors.I18nPreprocessor'],
+    MUSTACHEJS_REGEX_FINDERS=[
+        "mustachejs.finders.FilesystemRegexFinder",
+        "mustachejs.finders.AppRegexFinder",
+        ],
     )