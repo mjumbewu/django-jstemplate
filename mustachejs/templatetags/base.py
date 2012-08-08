01ea13c002e9e5f8e0a902ba74269dbba8fac91a
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2012-08-08 02:05:19 -0400

    Remove redundant, unused, incorrect mustacheraw filter in the base templatetag module.

--- mustachejs/templatetags/base.py
+++ mustachejs/templatetags/base.py
@@ -61,18 +61,3 @@ class BaseMustacheNode(template.Node):
 
     def generate_node_text(self, resolved_name, file_content):
         raise NotImplementedError()
-
-
-@register.tag
-def mustacheraw(parser, token):
-    """
-    Prints out raw mustache content
-
-    """
-    bits = token.contents.split()
-    if len(bits) not in [2, 3]:
-        raise template.TemplateSyntaxError(
-            "'mustacheraw' tag takes either (1) one argument: the name/id of "
-            "the template, or (2)  two arguments: the name of a subdirectory "
-            "to search and a regular expression of files to search for")
-    return MustacheRaw(*bits[1:])
