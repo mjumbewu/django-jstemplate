e692c152a170e4ba6cacdf84a1da1c353908eb6a
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2012-12-16 14:46:29 -0500

    Use bash instead of sh in install script

--- ci/install.sh
+++ ci/install.sh
@@ -1,11 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 PYTHON_VERSION=$(python --version 2>&1)
 
-if [[ "$PYTHON_VERSION" >= "Python 3" ]]
+if [[ "$PYTHON_VERSION" > "Python 3" ]]
 then
   if [[ "$DJANGO" < "Django==1.5" ]]
   then
+    echo "Cannot install $DJANGO on $PYTHON_VERSION"
     exit
   fi
 fi
