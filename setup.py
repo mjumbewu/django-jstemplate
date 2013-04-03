11b257c2211dbbb4852524882ea3ade6e56c7886
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2013-04-03 15:08:31 -0400

    Add Python 3.3 support explicitly

--- setup.py
+++ setup.py
@@ -43,6 +43,7 @@ setup(
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
         "Framework :: Django",
     ],
     zip_safe=False,
